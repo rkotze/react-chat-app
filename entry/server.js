b8eecdb67ac5b0faa5f3fe45a8585615ecd485f6
Richard Kotze <richkotze@outlook.com>
2018-04-25 00:13:32 +0100

    Got basics of chat app
    Rooms, user and messages.

--- entry/server.js
+++ entry/server.js
@@ -15,10 +15,40 @@ app.use(express.static('public'));
 
 app.get('*', start);
 
+const rooms = ['room1', 'room2', 'room3'];
+const usernames = {};
+
 io.on('connection', function (socket) {
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg);
+  socket.on('addUser', function (username) {
+    socket.username = username;
+    usernames[username] = username;
+    socket.room = 'room1';
+    socket.join(socket.room);
+    socket.emit('updateChat', 'SERVER', 'you have connected to room1');
+    socket.broadcast.to('room1').emit('updateChat', 'SERVER', username + ' has connected to this room');
+    socket.emit('updateRooms', rooms, 'room1');
   });
+
+  socket.on('sendChat', function (msg) {
+    io.sockets.in(socket.room).emit('updateChat', socket.username, msg);
+  });
+
+  socket.on('switchRoom', function (newroom) {
+    socket.leave(socket.room);
+    socket.join(newroom);
+    socket.emit('updateChat', 'SERVER', 'you have connected to ' + newroom);
+    socket.broadcast.to(socket.room).emit('updateChat', 'SERVER', socket.username + ' has left this room');
+    socket.room = newroom;
+    socket.broadcast.to(newroom).emit('updateChat', 'SERVER', socket.username + ' has joined this room');
+    socket.emit('updateRooms', rooms, newroom);
+  });
+
+  socket.on('disconnect', function () {
+    delete usernames[socket.username];
+    socket.broadcast.emit('updateChat', 'SERVER', socket.username + ' has disconnected');
+    socket.leave(socket.room);
+  });
+
 });
 
 const PORT = 3000;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ class Home extends React.Component {
   state = {
     messages: [],
     inputMessage: '',
-    status: 'no connection'
+    status: 'no connection',
+    groupList: []
   }
 
   socket = null;
@@ -16,11 +17,18 @@ class Home extends React.Component {
     this.socket = io('http://localhost:3000');
     this.socket.on('connect', () => {
       this.setState({ status: 'Connected: ' + this.socket.id });
+      this.socket.emit('addUser', prompt("What's your name?"));
     });
-    this.socket.on('chat message', (msg) => {
+    this.socket.on('updateChat', (username, msg) => {
       this.setState((prevState) => ({
-        messages: [...prevState.messages, msg]
+        messages: [...prevState.messages, username + ': ' + msg]
       }));
+    });
+    this.socket.on('updateRooms', (rooms, currentGroup) => {
+      this.setState({
+        groupList: rooms,
+        currentGroup: currentGroup
+      });
     });
   }
 
@@ -30,14 +38,19 @@ class Home extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.socket.emit('chat message', this.state.inputMessage);
+    this.socket.emit('sendChat', this.state.inputMessage);
     this.setState({
       inputMessage: ''
     });
   }
 
   render() {
-    const { inputMessage, messages, status } = this.state;
+    const { inputMessage,
+      messages,
+      status,
+      groupList,
+      currentGroup
+    } = this.state;
     return (
       <div className="home">
         <Helmet
@@ -46,12 +59,14 @@ class Home extends React.Component {
             { name: 'description', content: 'chat' }
           ]} />
         <ChatBox>
-          <Groups groupList={[{ name: "Default Group" }]} />
+          <Groups socket={this.socket} currentGroup={currentGroup} groupList={groupList} />
           <div className="message-box">
+
             <div className="message-list">
               <p>Status: {status}</p>
               {messages.map((message) => <p>{message}</p>)}
             </div>
+
             <form className="message-form" onSubmit={this.handleSubmit}>
               <input className="message-input" onChange={this.handleInputMessage} value={inputMessage} />
               <input type="submit" value="Send" />
@@ -65,6 +80,22 @@ class Home extends React.Component {
 }
 
 const ChatBox = ({ children }) => <div className="chat-box">{children}</div>;
-const Groups = ({ groupList }) => <div className="groups">{groupList.map((group) => <div key={group.name}>{group.name}</div>)}</div>;
+class Groups extends React.Component {
+  handleRoomClick = (roomName) => {
+    return () => {
+      this.props.socket.emit('switchRoom', roomName);
+    }
+  }
+
+  render() {
+    const { groupList } = this.props;
+    return (<div className="groups">
+      {groupList.map((group) =>
+        <div key={group} onClick={this.handleRoomClick(group)}>
+          {group}
+        </div>)}
+    </div>);
+  }
+}
 
 export default Home;
