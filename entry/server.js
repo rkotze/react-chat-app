dadcbbf15eab8c0e829f004c722fea372fba5654
Richard Kotze <richkotze@outlook.com>
2018-04-24 13:28:05 +0100

    Basic implementation of a chat app using sockets.

--- entry/server.js
+++ entry/server.js
@@ -2,16 +2,26 @@ require('babel-register')({
   presets: ['react', 'env', 'stage-2']
 });
 const express = require('express');
+const httpServer = require('http').Server;
+const socket = require('socket.io');
 
 const { start } = require('./react-server');
 
 const app = express();
+const http = httpServer(app);
+const io = socket(http);
 
 app.use(express.static('public'));
 
 app.get('*', start);
 
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
+});
+
 const PORT = 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('http://localhost:' + PORT);
 });
--- src/index.js
+++ src/index.js
@@ -1,23 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Helmet from 'react-helmet'
+import Helmet from 'react-helmet';
+import io from 'socket.io-client';
 
 class Home extends React.Component {
-  handleClick() {
-    alert('you clicked me');
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      inputMessage: '',
+      status: 'no connection'
+    };
+    this.socket;
+  }
+
+  componentDidMount() {
+    this.socket = io('http://localhost:3000');
+    this.socket.on('connect', () => {
+      this.setState({ status: 'Connected: ' + this.socket.id });
+    });
+    this.socket.on('chat message', (msg) => {
+      this.setState((prevState) => ({
+        messages: [...prevState.messages, msg]
+      }));
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  }
+
+  handleInputMessage = (e) => {
+    this.setState({ inputMessage: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.socket.emit('chat message', this.state.inputMessage);
+    this.setState({
+      inputMessage: ''
+    });
   }
 
   render() {
+    const { inputMessage, messages, status } = this.state;
     return (
       <div>
         <Helmet
-          title="Hello world, welcome!"
+          title="Chat"
           meta={[
-            { name: 'description', content: 'Hello world page' }
+            { name: 'description', content: 'chat' }
           ]} />
-        <h1>Hello world.</h1>
-        <p>Server side rendering <Link to="/about">About</Link> </p>
-        <button onClick={this.handleClick}>click</button>
+        <h1>Chat</h1>
+        <p>Status: {status}</p>
+        {messages.map((message) => <p>{message}</p>)}
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleInputMessage} value={inputMessage} />
+          <input type="submit" value="send" />
+        </form>
+
       </div>
     );
   }
